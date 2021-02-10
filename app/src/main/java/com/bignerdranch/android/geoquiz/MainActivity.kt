da927473eb22d64a5bb88f9cbf79ffb263810eff
PabloR <pablotomas_1@hotmail.com>
2021-02-10 12:17:33 -0300

    Improvements after pr comments

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
             Question(R.string.question_americas, true),
             Question(R.string.question_asia, true))
     private var currentIndex = 0
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -45,7 +46,8 @@ class MainActivity : AppCompatActivity() {
             if (currentIndex - 1 < 0) {
                 currentIndex = questionBank.size - 1
             } else {
-                currentIndex = (currentIndex - 1) % questionBank.size }
+                currentIndex = (currentIndex - 1) % questionBank.size
+            }
             updateQuestion()
         }
         questionTextView.setOnClickListener { view: View ->
@@ -54,10 +56,12 @@ class MainActivity : AppCompatActivity() {
         }
         updateQuestion()
     }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
+
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
         val messageResId = if (userAnswer == correctAnswer) {
