47dbfa6e1da83a2097632f0736be911e8f74462f
PabloR <pablotomas_1@hotmail.com>
2021-02-10 14:25:53 -0300

    Fixing final pr comments

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuestionsBank.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuestionsBank.kt
@@ -7,17 +7,12 @@ object QuestionsBank {
         Question(R.string.question_mideast, false),
         Question(R.string.question_africa, false),
         Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
+        Question(R.string.question_asia, true)
+    )
 
-    fun getBankSize(): Int {
-        return questionBank.size
-    }
+    fun getBankSize() = questionBank.size
 
-    fun getQuestionText(index: Int): Int {
-        return questionBank[index].textResId
-    }
+    fun getQuestionText(index: Int) = questionBank[index].textResId
 
-    fun getQuestionAnswer(index: Int): Boolean {
-        return questionBank[index].answer
-    }
+    fun getQuestionAnswer(index: Int) = questionBank[index].answer
 }
