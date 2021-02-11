6ffedfd7241424eb9b7679e44e149477398aabf6
PabloR <pablotomas_1@hotmail.com>
2021-02-11 13:55:39 -0300

    Fixing User Messages, binding reference, buttons states

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -7,13 +7,13 @@ import android.widget.*
 import androidx.lifecycle.ViewModelProviders
 import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 
-private const val KEY_INDEX = "index"
-private const val KEY_SCORE = "score"
-private const val KEY_TOTALANSWERS = "total_answers"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val keyIndex = "index"
+    private val keyScore = "score"
+    private val keyAnswersCounter = "answersCounters"
+    private var binding: ActivityMainBinding? = null
     private val quizViewModel: QuizViewModel by lazy {
         ViewModelProviders.of(this).get(QuizViewModel::class.java)
     }
@@ -23,61 +23,66 @@ class MainActivity : AppCompatActivity() {
         setupViewBinding()
         getSavedInstanceState(savedInstanceState)
         setupClickListener()
-        checkQuestion()
+        setupAnswerButtonState()
         updateQuestion()
     }
 
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
         super.onSaveInstanceState(savedInstanceState)
-        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
-        savedInstanceState.putInt(KEY_SCORE, quizViewModel.score)
-        savedInstanceState.putInt(KEY_TOTALANSWERS, quizViewModel.answersCounter)
+        savedInstanceState.putInt(keyIndex, quizViewModel.currentIndex)
+        savedInstanceState.putInt(keyScore, quizViewModel.score)
+        savedInstanceState.putInt(keyAnswersCounter, quizViewModel.answersCounter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 
     private fun setupViewBinding() {
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
+        val view = binding?.root
         setContentView(view)
     }
 
     private fun getSavedInstanceState(savedInstanceState: Bundle?) {
-        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        val currentIndex = savedInstanceState?.getInt(keyIndex, 0) ?: 0
         quizViewModel.currentIndex = currentIndex
-        val score = savedInstanceState?.getInt(KEY_SCORE, 0) ?: 0
+        val score = savedInstanceState?.getInt(keyScore, 0) ?: 0
         quizViewModel.score = score
-        val totalAnswers = savedInstanceState?.getInt(KEY_TOTALANSWERS, 0) ?: 0
+        val totalAnswers = savedInstanceState?.getInt(keyAnswersCounter, 0) ?: 0
         quizViewModel.answersCounter = totalAnswers
     }
 
     private fun setupClickListener() {
-        binding.trueButton.setOnClickListener { view: View ->
+        binding?.trueButton?.setOnClickListener { view: View ->
             setAnswer()
             checkAnswer(true)
             checkResult()
         }
-        binding.falseButton.setOnClickListener { view: View ->
+        binding?.falseButton?.setOnClickListener { view: View ->
             setAnswer()
             checkAnswer(false)
             checkResult()
         }
-        binding.nextButton.setOnClickListener {
+        binding?.nextButton?.setOnClickListener {
             moveToNext()
-            checkQuestion()
+            setupAnswerButtonState()
             updateQuestion()
         }
-        binding.prevButton.setOnClickListener {
+        binding?.prevButton?.setOnClickListener {
             moveToPrev()
-            checkQuestion()
+            setupAnswerButtonState()
             updateQuestion()
         }
-        binding.questionTextView.setOnClickListener { view: View ->
+        binding?.questionTextView?.setOnClickListener { view: View ->
             moveToNext()
-            checkQuestion()
+            setupAnswerButtonState()
             updateQuestion()
         }
     }
 
-    private fun checkQuestion() {
+    private fun setupAnswerButtonState() {
         if (quizViewModel.answers[quizViewModel.currentIndex]) {
             disableButtons()
         } else {
@@ -87,7 +92,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateQuestion() {
         val questionTextResId = QuestionsBank.getQuestionText(quizViewModel.currentIndex)
-        binding.questionTextView.setText(questionTextResId)
+        binding?.questionTextView?.setText(questionTextResId)
     }
 
     private fun setAnswer() {
@@ -100,9 +105,9 @@ class MainActivity : AppCompatActivity() {
         val messageResId: String
         if (userAnswer == correctAnswer) {
             quizViewModel.score++
-            messageResId = "Correct!"
+            messageResId = getString(R.string.correct_toast)
         } else {
-            messageResId = "Incorrect!"
+            messageResId = getString(R.string.incorrect_toast)
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
         disableButtons()
@@ -111,7 +116,7 @@ class MainActivity : AppCompatActivity() {
     private fun checkResult() {
         if (quizViewModel.answersCounter == QuestionsBank.getBankSize()) {
             val result = ((quizViewModel.score.toDouble() / quizViewModel.answersCounter) * 100)
-            var resultMessage = "Your punctuation is $result %"
+            var resultMessage = getString(R.string.quizResult).plus(result)
             Toast.makeText(this, resultMessage, Toast.LENGTH_SHORT).show()
         }
     }
@@ -130,17 +135,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun disableButtons() {
-        binding.trueButton.isClickable = false
-        binding.trueButton.alpha = 0.5f
-        binding.falseButton.isClickable = false
-        binding.falseButton.alpha = 0.5f
+        binding?.trueButton?.isEnabled = false
+        binding?.falseButton?.isEnabled = false
     }
 
     private fun enableButtons() {
-        binding.trueButton.isClickable = true
-        binding.trueButton.alpha = 1f
-        binding.falseButton.isClickable = true
-        binding.falseButton.alpha = 1f
+        binding?.trueButton?.isEnabled = true
+        binding?.falseButton?.isEnabled = true
     }
 }
 
